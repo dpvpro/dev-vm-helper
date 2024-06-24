ea8c348aa4dea4089f710cea8fb15eed21e4f987
Denis Proskurin <4201995+dpvpro@users.noreply.github.com>
2024-06-24 09:52:01 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module spacevm-dev-helper
+
+go 1.22.4
+
+require (
+	github.com/spf13/pflag v1.0.5
+	libvirt.org/go/libvirt v1.10003.0
+)
---
+++ helper.go
@@ -0,0 +1,292 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"libvirt.org/go/libvirt"
+)
+
+type VirtualMachineStatus string
+
+const (
+	VirtStatePending     = VirtualMachineStatus("Pending")     // VM was just created and there is no state yet
+	VirtStateRunning     = VirtualMachineStatus("Running")     // VM is running
+	VirtStateBlocked     = VirtualMachineStatus("Blocked")     // VM Blocked on resource
+	VirtStatePaused      = VirtualMachineStatus("Paused")      // VM is paused
+	VirtStateShutdown    = VirtualMachineStatus("Shutdown")    // VM is being shut down
+	VirtStateShutoff     = VirtualMachineStatus("Shutoff")     // VM is shut off
+	VirtStateCrashed     = VirtualMachineStatus("Crashed")     // Most likely VM crashed on startup cause something is missing.
+	VirtStateHybernating = VirtualMachineStatus("Hybernating") // VM is hybernating usually due to guest machine request
+)
+
+type VirtualMachineStateInfo struct {
+	State          VirtualMachineStatus
+	MaxMemoryBytes uint64
+	MemoryBytes    uint64
+	CpuTime        uint64
+	CpuCount       uint
+}
+
+// Versions - originally created for testing purposes, not actually something we would need.
+// var libvirtVersion = *pflag.Bool("libvirt-version", false, "Returns result with version of libvirt populated")
+// var virshVersion = *pflag.Bool("virsh-version", false, "Returns result with version of virsh populated")
+// var tarsvirtVersion = *pflag.Bool("tarsvirt-version", false, "Returns result with version of tarsvirt populated")
+
+// VirtualMachine commands
+var virtualMachineState = pflag.Bool("state", false, "Returns result with a current machine state")
+var virtualMachineSoftReboot = pflag.Bool("soft-reboot", false, "reboots a machine gracefully, as chosen by hypervisor. Returns result with a current machine state")
+var virtualMachineHardReboot = pflag.Bool("hard-reboot", false, "sends a VM into hard-reset mode. This is damaging to all ongoing file operations. Returns result with a current machine state")
+var virtualMachineShutdown = pflag.Bool("shutdown", false, "gracefully shuts down the VM. Returns result with a current machine state")
+var virtualMachineShutoff = pflag.Bool("shutoff", false, "kills running VM. Equivalent to pulling a plug out of a computer. Returns result with a current machine state")
+var virtualMachineStart = pflag.Bool("start", false, "starts up a VM. Returns result with a current machine state")
+var virtualMachinePause = pflag.Bool("pause", false, "stops the execution of the VM. CPU is not used, but memory is still occupied. Returns result with a current machine state")
+var virtualMachineResume = pflag.Bool("resume", false, "called after Pause, to resume the invocation of the VM. Returns result with a current machine state")
+var virtualMachineCreate = pflag.Bool("create", false, "creates a new machine. Requires --xml-template parameter. Returns result with a current machine state")
+var virtualMachineDelete = pflag.Bool("delete", false, "deletes an existing machine.")
+var virtualMachinesIps = pflag.Bool("ips", false, "show ip addresses vm on host.")
+
+var vm = pflag.String("vm", "", "vm of the machine to work with")
+var xmlTemplate = pflag.String("xml-template", "", "path to an xml template file that describes a machine. See qemu docs on xml templates.")
+
+var libvirtInstance *libvirt.Connect
+
+// TODO: cool things you can do with Domain, but do not know how to:
+// virDomainInterfaceAddresses - gets data about an IP addresses on a current interfaces. Mega-tool.
+// virDomainGetGuestInfo - full data about a config of the guest OS
+// virDomainGetState - provides the data about an actual domain state. Why is it shutoff or hybernating. Requires copious amount of magic fuckery to find out the actual reason with multiplication and matrix transforms, but can be translated into a redable form.
+func main() {
+
+	pflag.Parse()
+
+	virtinit()
+	defer libvirtInstance.Close()
+
+	switch {
+	case *virtualMachineState:
+		VirtualMachineState(*vm)
+	case *virtualMachineSoftReboot:
+		VirtualMachineSoftReboot(*vm)
+	case *virtualMachineHardReboot:
+		VirtualMachineHardReboot(*vm)
+	case *virtualMachineShutdown:
+		VirtualMachineShutdown(*vm)
+	case *virtualMachineShutoff:
+		VirtualMachineShutoff(*vm)
+	case *virtualMachineStart:
+		VirtualMachineStart(*vm)
+	case *virtualMachinePause:
+		VirtualMachinePause(*vm)
+	case *virtualMachineResume:
+		VirtualMachineResume(*vm)
+	case *virtualMachineCreate:
+		VirtualMachineCreate(*xmlTemplate)
+	case *virtualMachineDelete:
+		VirtualMachineDelete(*vm)
+	case *virtualMachinesIps:
+		VirtualMachinesIps()
+	}
+
+}
+
+// VirtualMachineState returns current state of a virtual machine.
+func VirtualMachineState(vm string) {
+	var ret VirtualMachineStateInfo
+
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	domainInfo, err := d.GetInfo()
+	herr(err)
+
+	ret.CpuCount = domainInfo.NrVirtCpu
+	ret.CpuTime = domainInfo.CpuTime
+	// god only knows why they return memory in kilobytes.
+	ret.MemoryBytes = domainInfo.Memory * 1024
+	ret.MaxMemoryBytes = domainInfo.MaxMem * 1024
+
+	switch domainInfo.State {
+	case libvirt.DOMAIN_NOSTATE:
+		ret.State = VirtStatePending
+	case libvirt.DOMAIN_RUNNING:
+		ret.State = VirtStateRunning
+	case libvirt.DOMAIN_BLOCKED:
+		ret.State = VirtStateBlocked
+	case libvirt.DOMAIN_PAUSED:
+		ret.State = VirtStatePaused
+	case libvirt.DOMAIN_SHUTDOWN:
+		ret.State = VirtStateShutdown
+	case libvirt.DOMAIN_SHUTOFF:
+		ret.State = VirtStateShutoff
+	case libvirt.DOMAIN_CRASHED:
+		ret.State = VirtStateCrashed
+	case libvirt.DOMAIN_PMSUSPENDED:
+		ret.State = VirtStateHybernating
+	}
+
+	hret(ret)
+}
+
+// VirtualMachineCreate creates a new VM from an xml template file
+func VirtualMachineCreate(xmlTemplate string) {
+
+	xml, err := os.ReadFile(xmlTemplate)
+	herr(err)
+
+	d, err := libvirtInstance.DomainDefineXML(string(xml))
+	herr(err)
+
+	hret(d)
+}
+
+// VirtualMachineDelete deletes a new VM from an xml template file
+func VirtualMachineDelete(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.UndefineFlags(libvirt.DOMAIN_UNDEFINE_KEEP_NVRAM)
+	herr(err)
+	hok(fmt.Sprintf("%v was deleted", vm))
+}
+
+// VirtualMachineSoftReboot reboots a machine gracefully, as chosen by hypervisor.
+func VirtualMachineSoftReboot(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Reboot(libvirt.DOMAIN_REBOOT_DEFAULT)
+	herr(err)
+
+	hok(fmt.Sprintf("%v was soft-rebooted successfully", vm))
+}
+
+// VirtualMachineHardReboot sends a VM into hard-reset mode. This is damaging to all ongoing file operations.
+func VirtualMachineHardReboot(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Reset(0)
+	herr(err)
+
+	hok(fmt.Sprintf("%v was hard-rebooted successfully", vm))
+}
+
+// VirtualMachineShutdown gracefully shuts down the VM.
+func VirtualMachineShutdown(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Shutdown()
+	herr(err)
+
+	hok(fmt.Sprintf("%v was shutdown successfully", vm))
+}
+
+// VirtualMachineShutoff kills running VM. Equivalent to pulling a plug out of a computer.
+func VirtualMachineShutoff(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Destroy()
+	herr(err)
+
+	hok(fmt.Sprintf("%v was shutoff successfully", vm))
+}
+
+// VirtualMachineStart starts up a VM.
+func VirtualMachineStart(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	//v.DomainRestore()
+	//_, err = v.DomainCreateWithFlags(d, uint32(libvirt.DomainStartBypassCache))
+	err = d.Create()
+	herr(err)
+
+	hok(fmt.Sprintf("%v was started", vm))
+}
+
+// VirtualMachinePause stops the execution of the VM. CPU is not used, but memory is still occupied.
+func VirtualMachinePause(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Suspend()
+	herr(err)
+
+	hok(fmt.Sprintf("%v is paused", vm))
+}
+
+// VirtualMachineResume can be called after Pause, to resume the invocation of the VM.
+func VirtualMachineResume(vm string) {
+	d, err := libvirtInstance.LookupDomainByName(vm)
+	herr(err)
+
+	err = d.Resume()
+	herr(err)
+
+	hok(fmt.Sprintf("%v was resumed", vm))
+}
+
+func VirtualMachinesIps() {
+
+	AllDomains, err := libvirtInstance.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE)
+	herr(err)
+
+	fmt.Printf("%d running domains:\n", len(AllDomains))
+	for _, domain := range AllDomains {
+		DomainName, err := domain.GetName()
+		herr(err)
+		// fmt.Printf("Domain - %s\n", domainName)
+
+		AllDomainInterfaces, err := domain.ListAllInterfaceAddresses(libvirt.DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE)
+		herr(err)
+		// fmt.Printf("Domain interfaces - %v, Type - %T\n", allDomainInterfaces, allDomainInterfaces)
+
+		for _, DomainInterfaceEntry := range AllDomainInterfaces {
+
+			var AllAddrs string
+			for _, val := range DomainInterfaceEntry.Addrs {
+				AllAddrs += val.Addr + " "
+			}
+			fmt.Printf("%s, interface - %v, ip address - %v\n",
+				DomainName, DomainInterfaceEntry.Name, AllAddrs)
+
+			// fmt.Printf("Domain - %s, interface - %v, ip address - %v\n",
+			//   domainName, domainInterfaceEntry.Name, domainInterfaceEntry.Addrs[0].Addr)
+		}
+
+		domain.Free()
+	}
+}
+
+func herr(e error) {
+	if e != nil {
+		fmt.Printf(`{"error":"%v"}`, strings.ReplaceAll(e.Error(), "\"", ""))
+		os.Exit(1)
+	}
+}
+
+func hok(message string) {
+	fmt.Printf(`{"ok":"%v"}`, strings.ReplaceAll(message, "\"", ""))
+	os.Exit(0)
+}
+
+func hret(i interface{}) {
+	ret, err := json.Marshal(i)
+	herr(err)
+	fmt.Print(string(ret))
+	os.Exit(0)
+}
+
+func virtinit() {
+	var err error
+	libvirtInstance, err = libvirt.NewConnect("qemu:///system")
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+}
